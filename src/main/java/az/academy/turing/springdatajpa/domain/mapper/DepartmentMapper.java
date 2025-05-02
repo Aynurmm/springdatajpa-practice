c21f995dd706e53a5a1707893411357039ddfe4d
Aynurmm <aynurmammadova09@gmail.com>
2025-05-02 16:19:47 +0400

    commit

--- src/main/java/az/academy/turing/springdatajpa/domain/mapper/DepartmentMapper.java
+++ src/main/java/az/academy/turing/springdatajpa/domain/mapper/DepartmentMapper.java
@@ -3,9 +3,11 @@ package az.academy.turing.springdatajpa.domain.mapper;
 
 import az.academy.turing.springdatajpa.domain.dto.response.DepartmentResponse;
 import az.academy.turing.springdatajpa.domain.model.Department;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
-public interface DepartmentMapper extends EntityMapper<Department, DepartmentResponse>{
+public interface DepartmentMapper extends EntityMapper <Department, DepartmentResponse> {
 
-    DepartmentMapper INSTANCE=  Mappers.getMapper(DepartmentMapper.class);
+    DepartmentMapper INSTANCE = Mappers.getMapper(DepartmentMapper.class);
 }
